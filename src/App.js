79ec71508e38e0a8bde36298ada2ea55caa5089e
George Yu <skylakebeta@gmail.com>
2021-09-03 00:00:27 -0700

    Add component library

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
+import ButtonPrimary from "@taskrabbit/meadow-web/lib/ButtonPrimary";
+import TRThemeProvider from "@taskrabbit/meadow-web/lib/Theme";
+
+import Button from "./components/Button";
 import logo from "./logo.svg";
 import "./App.css";
-import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button onClick={() => console.log("WAH")}>DOGE CI</Button>
-      </header>
-    </div>
+    <TRThemeProvider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Button onClick={() => console.log("WAH")}>DOGE CI</Button>
+          <ButtonPrimary>MEADOW</ButtonPrimary>
+        </header>
+      </div>
+    </TRThemeProvider>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("should render learn react link", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("should renders doge button", () => {
+    render(<App />);
+    const button = screen.getByText("DOGE CI");
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should renders meadow button", () => {
+    render(<App />);
+    const meadowButton = screen.getByText("MEADOW");
+    expect(meadowButton).toBeInTheDocument();
+  });
 });
